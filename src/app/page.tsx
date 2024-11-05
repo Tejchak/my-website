166d0fefc1efb135288d491d280d0b7ed4e57c54
Tejchak <chakravarthy.t@northeastern.edu>
2024-11-05 17:19:05 -0500

    added background and about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,31 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    //about link
+    <div className="relative">
+    <a
+      href="#about"
+      className="absolute top-4 right-4 text-sm sm:text-base text-blue-500 hover:underline"
+    >
+      About
+    </a>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
+    style={{
+      backgroundImage: "url('/me.jpeg')",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+    }}>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
+        {/* <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        /> */}
+        {/* <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
@@ -21,9 +35,9 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-        </ol>
+        </ol> */}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="flex gap-4 items-center flex-col sm:flex-row mt-64">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -96,6 +110,14 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+    </div>
+    <div id="about" className="mt-16 p-8">
+        <h2 className="text-2xl font-semibold">About Me</h2>
+        <p className="mt-4">
+          Tej Chakravarthy is a 2nd Year Computer Science Major studying at Northeastern University.
+          
+        </p>
+    </div>
     </div>
   );
 }
